1e6f71594b3cd6ec70cf4b8821f936814358826b
feiliu3k <1104289018@qq.com>
2016-11-10 15:17:37 +0800

    init

--- resources/views/admin/webinfo/index.blade.php
+++ resources/views/admin/webinfo/index.blade.php
@@ -40,34 +40,49 @@
                 </div>
             </div>
         </div>
-        <div class="panel panel-default">
-            <div class="panel-heading">评论列表</div>
-            <div class="panel-body">
-                <div class="col-md-12">
-                    <div class="main">
-                        <ul class="cbp_tmtimeline">
-                            @if ($live->comments)
-                                @foreach ($live->comments as $comment)
-                                <li>
-                                    <time class="cbp_tmtime" datetime="{{ $liveinfo->ifotime }}"><span>{{ $liveinfo->publishDate }}</span> <span>{{ $liveinfo->publishTime }}</span></time>
-                                    <div class="cbp_tmicon cbp_tmicon-phone"></div>
-                                    <div class="cbp_tmlabel">
-                                        <h2>{{$liveinfo->ifotitle}}
-                                            <div class="pull-right">
-                                            <a href="{{ url('/admin/weblive').'/'. $live->liveid.'/liveinfo/'.$liveinfo->ifoid.'/edit' }}" class="btn btn-xs btn-success">
-                                                <i class="fa fa-edit" ></i> 编辑
-                                            </a>
-                                            </div>
-                                        </h2>
-                                        {!! $liveinfo->ifocontent !!}
-                                    </div>
-                                </li>
-                                @endforeach
-                            @endif
-                        </ul>
-                    </div>
-                </div>
+        <div class="list-panel  panel panel-default">
+            <div class="panel-heading">
+                <div class="total">评论列表</div>
             </div>
+            <div class="panel-body">
+                <ul class="list-group row" >
+                 @if ($live->comments)
+                    @foreach ($live->comments as $comment)
+                        <li class="list-group-item media" style="margin-top: 0px;">
+                            <div class="infos">
+                                <div class="media-heading meta">
+                                    <span  class="remove-padding-left author">
+                                        {{$comment->userip}}
+                                    </span>
+                                    <span> • </span>
+                                    <abbr class="time" >{{$comment->ctime}}</abbr>
+                                    @if (Auth::check())
+                                        <span class="operate pull-right">
+                                            <button type="button" class="btn btn-danger btn-xs btn-delete" data-cid="{{ $comment->cid }}" data-tipid="{{ $comment->tipid }}" >
+                                                <i class="fa fa-times-circle"></i>
+                                                删除
+                                            </button>
+                                            <button type="button" class="btn btn-success btn-xs btn-verify" data-cid="{{ $comment->cid }}" data-tipid="{{ $comment->tipid }}" >
+                                                <i class="fa fa-check-square-o"></i>
+                                                @if ($comment->verifyflag==0)
+                                                    通过
+                                                @else
+                                                    取消
+                                                @endif
+                                            </button>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="media-body markdown-reply content-body">
+                                    <p> {{$comment->comment}}</p>
+                                </div>
+                            </div>
+                        </li>
+                    @endif
+                @endforeach
+                </ul>
+            </div>
+                {!! $live->comments()->orderby('ctime','desc')->paginate(config('weblive.posts_per_page'))->render() !!}
         </div>
     </div>
 @stop
--- weblive.sql
+++ weblive.sql
@@ -39,6 +39,30 @@ CREATE TABLE `password_resets` (
 
 /*Data for the table `password_resets` */
 
+/*Table structure for table `usercomment` */
+
+CREATE TABLE `usercomment` (
+  `ucid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `liveid` int(11) DEFAULT NULL COMMENT '关联weblive表的liveid',
+  `ucomment` varchar(2000) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '评论内容',
+  `ctime` datetime DEFAULT NULL COMMENT '发布时间',
+  `uid` int(11) DEFAULT NULL COMMENT 'cutv用户id号',
+  `nickname` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '昵称',
+  `mobile` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '手机号码',
+  `useravatar` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '用户头像',
+  `localrecord` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '用户标志',
+  `userip` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '用户ip',
+  `delflag` int(11) DEFAULT NULL COMMENT '删除标志（0是不删除，1是已删除）',
+  `verifyflag` int(11) DEFAULT NULL COMMENT '审核标志（0为不通过，1为通过）',
+  PRIMARY KEY (`ucid`),
+  KEY `delflag` (`delflag`),
+  KEY `verifyflag` (`verifyflag`),
+  KEY `liveid` (`liveid`),
+  KEY `ctime` (`ctime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `usercomment` */
+
 /*Table structure for table `users` */
 
 CREATE TABLE `users` (
@@ -68,9 +92,11 @@ CREATE TABLE `viewrecord` (
   `viewtime` datetime DEFAULT NULL COMMENT '首次观看时间',
   PRIMARY KEY (`id`),
   KEY `liveid` (`liveid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `viewrecord` */
+
+insert  into `viewrecord`(`id`,`liveid`,`localrecord`,`userip`,`userphone`,`viewtime`) values (7,1,'2016102711070867445','::1',NULL,'2016-10-31 10:11:43');
 
 /*Table structure for table `webifo` */
 
@@ -84,9 +110,11 @@ CREATE TABLE `webifo` (
   PRIMARY KEY (`ifoid`),
   KEY `liveid` (`liveid`),
   KEY `delflag` (`delflag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `webifo` */
+
+insert  into `webifo`(`ifoid`,`liveid`,`ifotitle`,`ifocontent`,`ifotime`,`delflag`) values (1,1,'豆腐干豆腐干豆腐干豆腐干豆','fgjhfghfghfghfgh山东分公司的分公司的分公司的分公司的风格山东分公司的分公司的分公司的分公司的分公司的风格山东分公司的风格','2016-10-25 15:40:13',0),(2,1,'678','gkjkgjkghjkgjkjh<img src=\"1.jpg\">','2016-10-26 10:58:12',0),(3,1,'23234','34234234','2016-10-28 09:47:22',0),(4,1,'222','3333','2016-10-29 17:04:33',0),(5,1,'333','345345345345345','2016-10-30 09:28:39',0),(6,1,'4444','56456456456456456','2016-10-30 10:28:55',0),(7,1,'55555','dfgdfgfgfdg','2016-10-30 11:29:14',0),(13,1,'666','22222','2016-10-30 12:28:55',0),(18,1,'7777','777777777777777','2016-11-01 16:41:39',0);
 
 /*Table structure for table `weblive` */
 
@@ -98,10 +126,13 @@ CREATE TABLE `weblive` (
   `livecontent` text COLLATE utf8_unicode_ci COMMENT '内容简介',
   `pnum` int(11) DEFAULT '0' COMMENT '观看人数',
   `readnum` int(11) DEFAULT '0' COMMENT '点击次数',
+  `commentflag` int(4) DEFAULT '0' COMMENT '评论标志（0无评论，1有评论）',
   PRIMARY KEY (`liveid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `weblive` */
+
+insert  into `weblive`(`liveid`,`livetitle`,`livetime`,`liveimg`,`livecontent`,`pnum`,`readnum`,`commentflag`) values (1,'123','2016-10-25 15:35:45',NULL,'asdfasdfasdf',6,201,1),(2,'234','2016-10-28 16:45:36',NULL,NULL,0,3,0);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
